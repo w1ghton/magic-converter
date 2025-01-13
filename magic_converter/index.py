c086baf24890d20791578bbfa59a5cd775c6ea66
Tim <you@example.com>
2025-01-13 20:08:54 +0300

    fixes

--- magic_converter/index.py
+++ magic_converter/index.py
@@ -1,20 +1,12 @@
 import requests
-
-from flask import (
-    Blueprint,
-    flash,
-    redirect,
-    render_template,
-    request,
-    url_for,
-)
+from flask import Blueprint, render_template, request
 
 bp = Blueprint("index", __name__, url_prefix="/")
 
 
 @bp.get("/")
 def index_get():
-    return render_template("index/index.html", title="Index")
+    return render_template("index/index.html", title="Magic Converter")
 
 
 @bp.post("/")
@@ -26,5 +18,5 @@ def index_post():
         + int(request.form.get("sickles", 0)) * 0.29
         + int(request.form.get("knuts", 0)) * 0.01
     )
-    rub = round(gbp * exchange_rate, 1)
-    return render_template("index/index.html", title="Index", rub=rub)
+    rub = f"{round(gbp * exchange_rate):,}".replace(",", " ")
+    return render_template("index/index.html", title="Magic Converter", rub=rub)

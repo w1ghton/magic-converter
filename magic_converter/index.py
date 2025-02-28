4f01cbd28f564c4a4760e777be2a681122f18761
Tim <github.smashup586@passinbox.com>
2025-02-28 17:09:02 +0300

    add items to section

--- magic_converter/index.py
+++ magic_converter/index.py
@@ -2,31 +2,24 @@ import requests
 from flask import Blueprint, render_template, request
 from .functions import get_currencies_list
 
-currencies = get_currencies_list()
-# print(currencies[0]['name'])
-
-
 bp = Blueprint("index", __name__, url_prefix="/")
-
 
 @bp.get("/")
 def index_get():
-    return render_template("index/index.html", title="Magic Converter")
-
+    currencies = get_currencies_list()
+    return render_template("index/index.html", title="Magic Converter", currencies=currencies)
 
 @bp.post("/")
-def index_post(currencies_list=currencies):
-    # data = requests.get("https://www.cbr-xml-daily.ru/daily_json.js").json()
-    # exchange_rate = data["Valute"]["GBP"]["Value"]
-    # try:
-    #     gbp = (
-    #         int(request.form.get("galleons", 0)) * 4.93
-    #         + int(request.form.get("sickles", 0)) * 0.29
-    #         + int(request.form.get("knuts", 0)) * 0.01
-    #     )
-    # except ValueError:
-    #     gbp = 0
+def index_post():
+    try:
+        gbp = (
+            int(request.form.get("galleons", 0)) * 4.93
+            + int(request.form.get("sickles", 0)) * 0.29
+            + int(request.form.get("knuts", 0)) * 0.01
+        )
+    except ValueError:
+        gbp = 0
+    exchange_rate = request.form.get("country")
+    print(exchange_rate)
     # rub = f"{round(gbp * exchange_rate):,}".replace(",", " ")
-    # currencies = get_currencies_list()
-    
-    return render_template("index/index.html", title="Magic Converter", currencies=currencies_list)
+    return render_template("index/index.html", title="Magic Converter")
